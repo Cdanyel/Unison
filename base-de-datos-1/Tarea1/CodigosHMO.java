1d2ebdb55fc718c30f364217c1e9a0ad1e1f5f60
Cdanyel <116329524+Cdanyel@users.noreply.github.com>
2026-01-15 23:26:57 -0700

    Add files via upload

--- base-de-datos-1/Tarea1/CodigosHMO.java
+++ base-de-datos-1/Tarea1/CodigosHMO.java
@@ -6,7 +6,8 @@ import java.util.HashMap;
 public class CodigosHMO {
 
     public static void main(String[] args) {
-
+ 
+        //Implementacion de HashMap, lectura de archivo y Linea de separación
         HashMap<String, Integer> codigos = new HashMap<>();
         String archivo = "codigos_postales_hmo.csv";
         String linea;
@@ -19,12 +20,12 @@ public class CodigosHMO {
 
             while ((linea = br.readLine()) != null) {
 
-                String[] datos = linea.split(",");
+                String[] datos = linea.split(","); // --Serapacion de datos por ´,´
 
-                String codigoPostal = datos[0];
+                String codigoPostal = datos[0]; // Lectura de primer dato del documento
 
                 if (codigos.containsKey(codigoPostal)) {
-                    codigos.put(codigoPostal, codigos.get(codigoPostal) + 1);
+                    codigos.put(codigoPostal, codigos.get(codigoPostal) + 1); // Codigo postal iguales sumar en los asentamientos
                 } else {
                     codigos.put(codigoPostal, 1);
                 }
@@ -33,14 +34,14 @@ public class CodigosHMO {
             br.close();
 
         } catch (IOException e) {
-            System.out.println("Error al lee+r el archivo");
+            System.out.println("Error al lee+r el archivo"); // Error al leer el archivo
         }
 
         System.out.println("Asentamientos por codigo postal:\n");
 
         for (String codigo : codigos.keySet()) {
             System.out.println(
-                "Codigo Postal: " + codigo +
+                "Codigo Postal: " + codigo +                             //imprimimos resultado establecido en el documento de la tarea.
                 " -> Total de asentamientos: " + codigos.get(codigo)
             );
         }
